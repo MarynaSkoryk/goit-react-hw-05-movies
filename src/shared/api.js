c8f233c10aadf93315380d1f9a78877b149c9899
Maryna Skoryk <skoryk.maryna@gmail.com>
2023-02-09 18:46:13 +0100

    add api

---
+++ src/shared/api.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: '000c6ed50406a56dccf609caa7f42d2c',
+    language: 'en-US',
+  },
+});
+
+export async function getTrendingMovies() {
+  const query = `/trending/movie/week`;
+  try {
+    const { data } = await instance.get(query);
+    return data.results;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getMovieDetails(id) {
+  const query = `/movie/${id}`;
+  try {
+    const { data } = await instance.get(query);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getMovieCast(id) {
+  const query = `/movie/${id}/credits`;
+  try {
+    const { data } = await instance.get(query);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getMovieReviews(id) {
+  const query = `/movie/${id}/reviews`;
+  try {
+    const { data } = await instance.get(query);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function searchMovie(queryString) {
+  const query = `/search/movie?query=${queryString}`;
+  try {
+    const { data } = await instance.get(query);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
